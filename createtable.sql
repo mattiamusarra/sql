a5b14fe31f802268474725fd3517cfb11e6ea36e
Mattia Musarra Tubbi <mattiamusarra@Air-di-Mattia.homenet.telecomitalia.it>
2024-05-26 17:37:09 +0200

    FINISCI SECONDA PARTE

--- createtable.sql
+++ createtable.sql
@@ -48,3 +48,76 @@ CREATE TABLE Luogo(
     foreign key(id_tipo_luogo) references TipoLuogo(id)
     foreign key(id_display) references Display(id)
 );
+CREATE TABLE Aula(
+    id int auto_increment PRIMARY KEY,
+    materia varchar(50) NOT NULL,
+    id_luogo int,
+
+    foreign key(id_luogo) references Luogo(id);
+);
+CREATE TABLE FasciaOraria(
+     id int auto_increment PRIMARY KEY,
+     oraInizio TIME NOT NULL,
+     oraFine TIME NOT NULL,
+     giorno varchar(50) NOT NULL
+);
+CREATE TABLE Utente(
+    id int auto_increment PRIMARY KEY,
+    nome varchar(100) NOT NULL,
+    cognome varchar(100) NOT NULL,
+    codice_fiscale varchar(16) UNIQUE,
+    eta int NOT NULL,
+    username varchar(100) NOT NULL,
+    email varchar(150) UNIQUE,
+    password varchar(150) NOT NULL
+);
+CREATE TABLE Dipendete(
+    id_utente int,
+    codice_contratto varchar(100) NOT NULL,
+    dipartimento varchar(100) NOT NULL,
+    stipendio float NOT NULL,
+    dataAssunzione DATE NOT NULL,
+    iban varchar(100) NOT NULL
+
+    foreign key(id_utente) references Utente(id),
+    PRIMARY KEY(id_utente,codice_contratto)
+);
+CREATE TABLE Orario(
+    id int auto_increment PRIMARY KEY,
+    giorno varchar(50) NOT NULL,
+    ora_apertura TIME NOT NULL,
+    ora_chiusura TIME NOT NULL
+);
+CREATE TABLE UfficioDocente(
+    id int auto_increment PRIMARY KEY,
+    id_orario int,
+    id_luogo int,
+
+    foreign key(id_orario) references Orario(id),
+    foreign key(id_luogo) references Luogo(id)
+);
+CREATE TABLE Docente(
+    id_utente int,
+    codice_contratto varchar(100) NOT NULL,
+    ruolo varchar(100) NOT NULL,
+    id_ufficio_docente int,
+
+    foreign key(id_ufficio_docente) references UfficioDocente(id),
+    foreign key(id_utente) references Utente(id),
+    PRIMARY KEY(id_utente,codice_contratto)
+);
+CREATE TABLE Lezioni(
+    id int auto_increment PRIMARY KEY,
+    materia varchar(100) NOT NULL,
+    descrizione varchar(200),
+    lingua varchar(50) NOT NULL,
+    id_aula int,
+    id_fascia_oraria int,
+    id_dipendente int,
+    codice_contratto varchar(100),
+
+    foreign key(id_aula) references Aula(id);
+    foreign key(id_fascia_oraria) references FasciaOraria(id);
+    foreign key(id_dipendente) references Dipendente(id);
+    foreign key(codice_contratto) references Dipendente(codice_contratto);
+)
